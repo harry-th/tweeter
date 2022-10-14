4738da8caa1f55b1ace1f0ff8453067b5a26b07e
harry thomas <harryt1905@gmail.com>
2022-10-13 21:33:45 -0400

    add error message on tweet length too long or short

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -36,6 +36,7 @@ let renderTweets = function(allTweets) {
     $('.tweet-container').prepend(renTweet);
   }
 };
+
 let scrollButton = () => {
   if ($(window).scrollTop() > 500) {
     $('#upButton').show();
@@ -45,32 +46,53 @@ let scrollButton = () => {
 };
 
 $(document).ready(function() {
+  $('#error-message').hide();
+
+  // top of the page button
+  //is hidden and counts scroll depth to figures if should display
   $('#upButton').hide();
-  $('#upButton').on('click', ()=> $(window).scrollTop(0,0));
-  $(window).on('scroll',scrollButton);
+  $(window).on('scroll', scrollButton);
+  $('#upButton').on('click', () => $(window).scrollTop(0, 0));
+
+
+
   $('#tweetForm').on('submit', function(e) {
-    let info = $(this).serialize();
-    $.post('/tweets', info).then(()=>{
-      $.get('/tweets', (data) => {
-        $('.tweet-container').empty();
-        renderTweets(data);
-      }).then(() => {
-        $('article:first-child').css('top', '-205px').css('opacity', '0').animate({
-          top:'+=10px',
-          opacity: '+=1'
-        }, 700, 'linear');
-        $('article').css('top', '-195px').animate({
-          top:'+=195px',
-        }, 1400, 'linear');
-      });
-    });
     e.preventDefault();
+    let info = $(this).serialize();
+    //check tweet length hides/show error messages
+    if (info.split('=')[1].length > 140 || info.split('=')[1].length === 0) {
+      $('#tweetForm').hide();
+      $('#error-message').slideDown('medium');
+      setTimeout(()=>{
+        $('#tweetForm').fadeIn();
+        $('#error-message').hide();
+      },4000);
+    } else {
+      //adds the new tweet then retrives the collection of tweets and animates the addition
+      $.post('/tweets', info).then(() => {
+        $.get('/tweets', (data) => {
+          $('.tweet-container').empty();
+          renderTweets(data);
+        }).then(() => {
+        //animation new tweet
+          $('article:first-child').css('top', '-205px').css('opacity', '0').animate({
+            top: '+=10px',
+            opacity: '+=1'
+          }, 700, 'linear');
+          $('article').css('top', '-195px').animate({
+            top: '+=195px',
+          }, 1400, 'linear');
+        });
+      });
+    }
   });
-  $.get('/tweets', (data)=>{
+  //initial get for tweet
+  $.get('/tweets', (data) => {
     renderTweets(data);
   });
+  //hide tweet form and toggle show on click
   $('.new-tweet').hide();
-  $('#dropTweet').on('click',()=>{
+  $('#dropTweet').on('click', () => {
     $('.new-tweet').slideToggle('slow');
   });
 });
